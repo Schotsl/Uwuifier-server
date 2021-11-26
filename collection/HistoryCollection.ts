f74159f35090cbae2f0b4e5af5c5501307595892
Sjors van Holst <sjorsvanholst@gmail.com>
2021-11-26 16:58:02 +0100

    ✏️ Fixed typo

--- collection/HistoryCollection.ts
+++ collection/HistoryCollection.ts
@@ -1,6 +1,6 @@
 import BaseCollection from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/collection/BaseCollection.ts";
-import HistortyEntity from "../entity/HistoryEntity.ts";
+import HistoryEntity from "../entity/HistoryEntity.ts";
 
 export default class HistoryCollection extends BaseCollection {
-  public histories: HistortyEntity[] = [];
+  public histories: HistoryEntity[] = [];
 }
