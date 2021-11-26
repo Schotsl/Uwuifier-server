9ec499a1479b2e06c7fbbdc09e2c2125be7b1170
Sjors van Holst <sjorsvanholst@gmail.com>
2021-11-26 16:58:16 +0100

    🩹 Forgotten files

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+// Load public IPv4 file so its in cache
+import "./ipv4.ts";
+
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { Application } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 import {
@@ -6,9 +9,9 @@ import {
   postHandler,
 } from "https://raw.githubusercontent.com/Schotsl/Uberdeno/main/middleware.ts";
 
+import originRouter from "./router/originRouter.ts";
 import historyRouter from "./router/historyRouter.ts";
 import translateRouter from "./router/translateRouter.ts";
-
 const application = new Application();
 
 application.use(oakCors());
@@ -17,10 +20,14 @@ application.use(errorHandler);
 application.use(limitHandler);
 application.use(postHandler);
 
+application.use(originRouter.routes());
 application.use(translateRouter.routes());
 application.use(historyRouter.routes());
 
+application.use(originRouter.allowedMethods());
 application.use(translateRouter.allowedMethods());
 application.use(historyRouter.allowedMethods());
 
 application.listen({ port: 8080 });
+
+type Origin = "website" | "android" | "endpoint";
